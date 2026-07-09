4a1cb229ddf4ef97cebba5d74fba83e3db34076e
Jianguo Fan <754879794@qq.com>
2026-07-09 11:07:51 +0800

    更换ip

--- lib/features/farm/presentation/pages/farm_dashboard_page.dart
+++ lib/features/farm/presentation/pages/farm_dashboard_page.dart
@@ -67,7 +67,7 @@ class _FarmDashboardPageState extends ConsumerState<FarmDashboardPage> {
 
     try {
       await manager.connect(
-        host: '172.18.2.178',
+        host: '172.17.11.209',
         port: 1883,
         username: 'lava_app',
         password: 'lava-farm-admin',
